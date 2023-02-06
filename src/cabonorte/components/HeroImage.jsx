b69452bc24ae70b78b57f2a6cadfe9f08bd5035d
Raül de Arriba <rauldearriba@gmail.com>
2023-02-06 13:36:38 +0100

    hero

--- src/cabonorte/components/HeroImage.jsx
+++ src/cabonorte/components/HeroImage.jsx
@@ -8,8 +8,10 @@ export const HeroImage = (props) => {
       <div className="container">
         <div className="row align-self-center my-align-vertical text-left">
           <div className="col bg-transparent">
+            {/*
               <h1>CABO NORTE</h1>
               <h2>Alimentación canina</h2>
+            */}
           </div>
         </div>
       </div>
